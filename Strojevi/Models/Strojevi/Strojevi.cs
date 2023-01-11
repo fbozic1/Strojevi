48db43ac06947d770441f8ce08e8cb305ee21c96
Bam2bus <Bam2bus@DESKTOP-KC2U7S7>
2023-01-11 18:03:00 +0100

    Add project files.

---
+++ Strojevi/Models/Strojevi/Strojevi.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace Strojevi.Models
+{
+    public class Strojevi
+    {
+        public int strojeviid { get; set; }
+
+        public string naziv { get; set; }
+    }
+}
